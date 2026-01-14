15f49949593048d05fc809af291be297bbe8c77c
João Lucas <lgbr212@gmail.com>
2026-01-14 16:07:19 -0300

    feat(server): Adicionando rotas ao server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import ProductRoutes from "./routes/product.route.js";
-import UserRoutes from "./routes/user.route.js";
+import ProductRoutes from "./routes/product.routes.js";
+import UserRoutes from "./routes/user.routes.js";
+import GoalRoutes from "./routes/goal.routes.js";
+import RegisterRoutes from "./routes/register.routes.js";
+import EmployeeRoutes from "./routes/employee.routes.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/products", ProductRoutes);
 app.use("/users", UserRoutes);
+app.use("/goals", GoalRoutes);
+app.use("/registers", RegisterRoutes);
+app.use("/employees", EmployeeRoutes);
 
 app.get("/", (req: Request, res: Response) => res.json({ status: "ok" }));
 
