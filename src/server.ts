768233f9bf9b397c077884ccf78b4c46d5fa3aa7
João Lucas <lgbr212@gmail.com>
2026-01-14 14:05:41 -0300

    feat(server): Adicionando rota /users

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ProductRoutes from "./routes/product.route.js";
+import UserRoutes from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/products", ProductRoutes);
+app.use("/users", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => res.json({ status: "ok" }));
 
