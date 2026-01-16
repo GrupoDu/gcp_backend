87919e37e5e7722b897de01c1ebec2d9b98cdf13
João Lucas <lgbr212@gmail.com>
2026-01-16 16:04:03 -0300

    feat(userValidation): Tipagem e validação de tipos para middleware de autenticação

---
+++ src/types/userValidation.interface.ts
@@ -0,0 +1,22 @@
+import type { IUser } from "./models.interface.js";
+
+export interface IUserValidation {
+  success: boolean;
+  user?: IUser;
+  error?: {
+    message: string;
+    statusCode: number;
+  };
+}
+
+export function successfulValidation(
+  validation: IUserValidation
+): validation is IUserValidation & { success: true; user: IUser } {
+  return validation.success === true && validation.user !== undefined;
+}
+
+export function passwordSuccessfulValidation(
+  validation: IUserValidation
+): validation is IUserValidation & { success: true; user: IUser } {
+  return validation.success === true && validation.user !== undefined;
+}
