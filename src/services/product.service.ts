ad619d39cd27df12b65903ccc13e7f66232df245
João Lucas <lgbr212@gmail.com>
2026-01-21 08:02:48 -0300

    refactor(product): Implementando novos tipos

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,45 +1,41 @@
 import { responseMessages } from "../constants/messages.constants.js";
-import type { IProductResponse } from "../types/product.interface.js";
-import type { IProductInputData } from "../types/product.interface.js";
+import type {
+  IProduct,
+  IProductCreate,
+  IProductUpdate,
+} from "../types/product.interface.js";
 import type { PrismaClient } from "@prisma/client";
 
 class ProductService {
   constructor(private prisma: PrismaClient) {}
 
-  async getAllProductsData(): Promise<IProductResponse[]> {
-    const allProducts: IProductResponse[] =
-      await this.prisma.products.findMany();
+  async getAllProductsData(): Promise<IProduct[]> {
+    const allProducts: IProduct[] = await this.prisma.products.findMany();
 
     return allProducts;
   }
 
-  async registerNewProduct(
-    newProductData: IProductInputData,
-  ): Promise<IProductResponse> {
+  async registerNewProduct(newProductData: IProductCreate): Promise<IProduct> {
     const { name, description, product_type, image } = newProductData;
 
     if (!name || !description || !product_type || !image) {
       throw new Error(responseMessages.fillAllFieldMessage);
     }
 
-    const newProduct: IProductResponse = await this.prisma.products.create({
+    const newProduct: IProduct = await this.prisma.products.create({
       data: newProductData,
     });
 
     return newProduct;
   }
 
-  async updateProductData(
-    productNewData: IProductInputData,
-    productUuid: string,
-  ): Promise<IProductResponse> {
-    if (!productNewData || !productUuid) {
+  async updateProductData(productNewData: IProductUpdate): Promise<IProduct> {
+    if (!productNewData.uuid)
       throw new Error(responseMessages.fillAllFieldMessage);
-    }
 
-    const updatedProduct: IProductResponse = await this.prisma.products.update({
+    const updatedProduct: IProduct = await this.prisma.products.update({
       where: {
-        uuid: productUuid,
+        uuid: productNewData.uuid,
       },
       data: productNewData,
     });
