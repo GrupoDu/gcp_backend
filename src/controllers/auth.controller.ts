cf382d5e221605963ec00eb122b0e79fe1ba2013
João Lucas <lgbr212@gmail.com>
2026-01-22 14:52:02 -0300

    refactor(auth): Alterando nome do arquivo e implementando service dentro do controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,35 @@
+import type { Request, Response } from "express";
+import { responseMessages } from "../constants/messages.constants.js";
+import type { IUserPublic } from "../types/user.interface.js";
+import type AuthService from "../services/auth.service.js";
+
+class AuthController {
+  constructor(private authService: AuthService) {}
+
+  async userLogin(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      const userLoged: IUserPublic = await this.authService.userLogin(
+        email,
+        password,
+      );
+
+      const token = await this.authService.generateAccessToken(
+        userLoged.user_id,
+      );
+
+      return res.status(200).json({
+        message: `Usuário logado com sucesso.`,
+        token: token,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+}
+
+export default AuthController;
