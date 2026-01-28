8353176c2f64f265be2a4b2c82e414ec26c11e2a
João Lucas <lgbr212@gmail.com>
2026-01-28 16:52:12 -0300

    test(registter): Adicionando teste de update e importando dado mockados

--- src/services/__tests__/register.service.test.ts
+++ src/services/__tests__/register.service.test.ts
@@ -1,12 +1,13 @@
 import { describe, it, beforeEach, expect, vi } from "vitest";
 import RegisterService from "../register.service.js";
-import type { IRegister } from "../../types/register.interface.js";
 import { Prisma } from "@prisma/client";
 
 vi.mock("../../../lib/prisma.js");
 
 import prisma from "../../tests/__mocks__/@prisma/prisma.js";
 import { randomUUID } from "node:crypto";
+import type { IRegister } from "../../types/register.interface.js";
+import { mockedRegisterFactory } from "../../tests/factories/register.factory.js";
 
 describe("Testes de criação de registro.", () => {
   let registerService: RegisterService;
@@ -17,31 +18,39 @@ describe("Testes de criação de registro.", () => {
   });
 
   it("Deve conseguir criar um novo registro.", async () => {
-    const newMockedRegister = {
-      register_id: randomUUID(),
-      created_at: new Date(),
-      title: "Título do registro",
-      deadline: new Date(2025, 3, 5),
-      description:
-        "Descrição do registro com observações e detalhes importantes",
-      status: "Pendente",
-      client_uuid: "03899d9f-0c34-4458-8883-1e8523bc14b5",
-      product_quantity: new Prisma.Decimal("100.00"),
-      product_uuid: "a25a26f7-0e2c-48e9-a7e2-b92772784f87",
-      employee_uuid: null,
-      cut_assistant: null,
-      deliver_observation: null,
-      delivered_at: null,
-      finishing_assistant: null,
-      fold_assistant: null,
-      paint_assistant: null,
-    };
+    const mockedRegister = mockedRegisterFactory();
 
-    prisma.register.create.mockResolvedValue(newMockedRegister);
+    prisma.register.create.mockResolvedValue(mockedRegister);
 
-    const newRegister =
-      await registerService.createNewRegister(newMockedRegister);
+    const newRegister = await registerService.createNewRegister(mockedRegister);
 
     expect(newRegister.title).toBe("Título do registro");
+  });
+});
+
+describe("Testes de update de registro.", () => {
+  let registerService: RegisterService;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    registerService = new RegisterService(prisma);
+  });
+
+  it("Deve editar nome de um registro.", async () => {
+    const mockedUpdateRegister = mockedRegisterFactory({
+      title: "Update de nome",
+    });
+
+    prisma.register.update.mockResolvedValue(mockedUpdateRegister);
+
+    const updateRegister = await registerService.updateRegisterData(
+      mockedUpdateRegister,
+      "550e8400-e29b-41d4-a716-446655440000",
+    );
+
+    expect(updateRegister.title).toBe("Update de nome");
+    expect(updateRegister.register_id).toBe(
+      "550e8400-e29b-41d4-a716-446655440000",
+    );
   });
 });
