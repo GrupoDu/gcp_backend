706eb967c312576ec51bfb6576d472ae54143c6d
João Lucas <lgbr212@gmail.com>
2026-02-12 17:01:40 -0300

    refactor(productionOrder): Alterando nome de register para production_order

---
+++ src/controllers/productionOrder.controller.ts
@@ -0,0 +1,115 @@
+import type { Request, Response } from "express";
+import type ProductionOrderService from "../services/productionOrder.service.js";
+import { responseMessages } from "../constants/messages.constants.js";
+
+class ProductionOrderController {
+  constructor(private productionOrderService: ProductionOrderService) {}
+
+  async getAllProductionRegisters(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    try {
+      const allProductionRegisters =
+        await this.productionOrderService.getAllProductionOrders();
+
+      return res.status(200).json(allProductionRegisters);
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+
+  async getProductionOrderById(req: Request, res: Response): Promise<Response> {
+    const { uuid } = req.params;
+
+    try {
+      if (!uuid) throw new Error("id do registro não informado.");
+
+      const targetTask =
+        await this.productionOrderService.getProductionOrderById(
+          uuid as string,
+        );
+
+      return res.status(200).json(targetTask);
+    } catch (err) {
+      return res.status(500).json({
+        message: "Erro interno de servidor.",
+        error: (err as Error).message,
+      });
+    }
+  }
+
+  async createNewProductionOrder(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    try {
+      const newTaskValues = req.body;
+
+      const newTask =
+        await this.productionOrderService.createNewProductionOrder(
+          newTaskValues,
+        );
+
+      return res.status(201).json({
+        message: "Ordem de produção criado com sucesso.",
+        register: newTask,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+
+  async removeTask(req: Request, res: Response): Promise<Response> {
+    try {
+      const { uuid } = req.params;
+
+      if (!uuid)
+        return res
+          .status(422)
+          .json({ message: responseMessages.fillAllFieldMessage });
+
+      await this.productionOrderService.removeProductionOrder(uuid as string);
+
+      return res
+        .status(200)
+        .json({ message: "Ordem de produção deletada com sucesso. " });
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+
+  async updateProductionOrder(req: Request, res: Response): Promise<Response> {
+    try {
+      const ProductionOrderNewValues = req.body;
+      const { uuid } = req.params;
+
+      const updatedProductionOrder =
+        await this.productionOrderService.updateProductionOrder(
+          ProductionOrderNewValues,
+          uuid as string,
+        );
+
+      return res.status(200).json({
+        message: "Registro atualizado com sucesso.",
+        update: updatedProductionOrder,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+}
+
+export default ProductionOrderController;
---
+++ src/routes/productionOrder.routes.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import ProductionOrderController from "../controllers/productionOrder.controller.js";
+import type { Request, Response } from "express";
+import ProductionOrderService from "../services/productionOrder.service.js";
+import { prisma } from "../../lib/prisma.js";
+import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
+
+const router = express.Router();
+const productionOrderService = new ProductionOrderService(prisma);
+const productionOrderController = new ProductionOrderController(
+  productionOrderService,
+);
+
+router.get("/", (req: Request, res: Response) =>
+  productionOrderController.getAllProductionRegisters(req, res),
+);
+router.post("/", adminAuthMiddleware, (req: Request, res: Response) =>
+  productionOrderController.createNewProductionOrder(req, res),
+);
+router.get("/:uuid", adminAuthMiddleware, (req: Request, res: Response) =>
+  productionOrderController.getProductionOrderById(req, res),
+);
+router.delete("/:uuid", adminAuthMiddleware, (req: Request, res: Response) =>
+  productionOrderController.removeTask(req, res),
+);
+router.put("/:uuid", (req: Request, res: Response) =>
+  productionOrderController.updateProductionOrder(req, res),
+);
+
+export default router;
