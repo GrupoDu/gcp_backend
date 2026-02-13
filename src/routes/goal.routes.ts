b080e364c577edcfb66894e012c8ece5b004546a
João Lucas <lgbr212@gmail.com>
2026-02-13 10:38:32 -0300

    feat(goal): Adicionando middleware de autorização na rota de GET

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -3,12 +3,13 @@ import GoalController from "../controllers/goal.controller.js";
 import GoalService from "../services/goal.service.js";
 import { prisma } from "../../lib/prisma.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 const goalService = new GoalService(prisma);
 const goalController = new GoalController(goalService);
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", authMiddleware, (req: Request, res: Response) => {
   goalController.getAllGoalsData(req, res);
 });
 router.post("/", adminAuthMiddleware, (req: Request, res: Response) => {
