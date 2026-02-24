efadfe95a5bff21e44dbcef9b48c400abe4cf9f1
João Lucas <lgbr212@gmail.com>
2026-02-24 16:12:01 -0300

    feat(server): Adicionando mais uma origem permitida no cors

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://192.168.1.8:3001", "http://localhost:3000"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
