ac65e7c6b363ced5d3f09d7e78f46a54dae43cf1
João Lucas <lgbr212@gmail.com>
2026-02-25 17:14:53 -0300

    remove(getToken): Removendo linha de log

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -4,7 +4,6 @@ export function getToken(req: Request, res: Response): string {
   const auth = req.cookies?.access_token;
 
   if (!auth) {
-    console.log("Cookie token não encontrado. req.cookies:", req.cookies);
     throw new Error("TOKEN_NOT_PROVIDED");
   }
 
