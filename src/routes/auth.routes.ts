46bfc99f979b45c30628c7de3d17cdb50d2fd58f
João Lucas <lgbr212@gmail.com>
2026-02-27 11:01:28 -0300

    refactor(auth): Adicionando middleware getTokenMiddleware e accessTokenMiddleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "../middlewares/auth.middleware.ts";
 import { prisma } from "../../lib/prisma.ts";
 import AuthService from "../services/auth.service.ts";
 import accessTokenMiddleware from "../middlewares/accessToken.middleware.ts";
+import { getTokenMiddleware } from "../middlewares/getToken.middleware.ts";
 
 const router: Router = express.Router();
 const authService = new AuthService(prisma);
@@ -16,11 +17,14 @@ router.post("/", authMiddleware, (req: Request, res: Response) =>
 router.post("/refresh", (req: Request, res: Response) =>
   authController.refresh(req, res),
 );
-router.get("/verify", (req: Request, res: Response) =>
+router.get("/verify", accessTokenMiddleware, (req: Request, res: Response) =>
   authController.isTokenStillValid(req, res),
 );
-router.post("/logout", accessTokenMiddleware, (req: Request, res: Response) =>
-  authController.userLogout(req, res),
+router.post(
+  "/logout",
+  getTokenMiddleware,
+  accessTokenMiddleware,
+  (req: Request, res: Response) => authController.userLogout(req, res),
 );
 
 export default router;
