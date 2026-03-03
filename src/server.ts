53a7afdfdc58fff554dad9d53a71ffcbefc2378b
João Lucas <lgbr212@gmail.com>
2026-03-03 09:13:05 -0300

    feat(server): Separando portas em consts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import { Server } from "socket.io";
 import { httpServer, PORT } from "./app.ts";
 
+const HTTPS_PORT = 8003;
+const HTTP_PORT = 8001;
+
 export const io = new Server(httpServer, {
   cors: {
-    origin: ["https://192.168.1.7:3003", "http://localhost:3002"],
+    origin: [
+      `https://192.168.1.7:${HTTPS_PORT}`,
+      `http://localhost:${HTTP_PORT}`,
+    ],
     credentials: true,
   },
 });
