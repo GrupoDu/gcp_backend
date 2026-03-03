78893a4901f8c7e778912ba3a9a728ef45062a7c
João Lucas <lgbr212@gmail.com>
2026-03-03 08:19:36 -0300

    refactor(getToken): Removendo leitura de refresh token

--- src/middlewares/getToken.middleware.ts
+++ src/middlewares/getToken.middleware.ts
@@ -1,5 +1,4 @@
 import type { NextFunction, Request, Response } from "express";
-import { tryRefreshToken } from "../utils/tryRefreshToken.ts";
 import { tryAccessToken } from "../utils/tryAccessToken.ts";
 
 export function getTokenMiddleware(
@@ -8,13 +7,12 @@ export function getTokenMiddleware(
   next: NextFunction,
 ) {
   const accessToken = req.cookies.access_token;
-  const refreshToken = req.cookies.refresh_token;
 
   // Primeiro, tenta ver se o access_token é válido
   if (accessToken) {
     const accessResult = tryAccessToken(accessToken);
     if (accessResult.isValid) {
-      console.log("Usando access_token válido");
+      // console.log("Usando access_token válido");
       req.tokenResponse = {
         token: accessToken,
         payload: accessResult,
@@ -22,22 +20,7 @@ export function getTokenMiddleware(
       };
       return next();
     }
-    console.log("Access_token expirado ou inválido");
-  }
-
-  // Se access_token não é válido, tenta refresh_token
-  if (refreshToken) {
-    const refreshResult = tryRefreshToken(refreshToken);
-    if (refreshResult.isValid) {
-      console.log("Usando refresh_token válido");
-      req.tokenResponse = {
-        token: refreshToken,
-        payload: refreshResult,
-        token_type: "refresh",
-      };
-      return next();
-    }
-    console.log("Refresh_token expirado ou inválido");
+    // console.log("Access_token expirado ou inválido");
   }
 
   console.log("Nenhum token válido encontrado");
