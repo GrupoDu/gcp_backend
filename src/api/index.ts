0ace1ca143887c7dba6b9791202621d77b85aacb
João Lucas <lgbr212@gmail.com>
2026-03-05 11:58:57 -0300

    remove: Removendo pasta api

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Api } from "./Api.js";
-
-// Exporta a API já configurada
-export const api = new Api({
-  baseUrl: "https://gcp-backend-gnxf.onrender.com",
-});
-
-// Exporta os tipos também
-export * from "./Api.js";
