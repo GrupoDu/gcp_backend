6d377d37ebc068067c285f22f1d6966ac2d21976
jukalbf <lgbr212@gmail.com>
2026-03-30 14:21:45 -0300

    refactor(product): Removendo import de express

--- src/routes/productionOrder.routes.ts
+++ src/routes/productionOrder.routes.ts
@@ -1,12 +1,11 @@
-import express from "express";
-import type { Request, Response, Router } from "express";
+import { Router, type Request, type Response } from "express";
 import ProductionOrderController from "../controllers/productionOrder.controller.js";
 import ProductionOrderService from "../services/productionOrder.service.js";
 import { prisma } from "../../lib/prisma.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
 import { getTokenMiddleware } from "../middlewares/getToken.middleware.js";
 
-const router: Router = express.Router();
+const router: Router = Router();
 const productionOrderService = new ProductionOrderService(prisma);
 const productionOrderController = new ProductionOrderController(
   productionOrderService,
@@ -37,12 +36,12 @@ router.put(
   (req: Request, res: Response) =>
     productionOrderController.updateProductionOrder(req, res),
 );
-router.put(
-  "/deliver/:production_order_id",
-  getTokenMiddleware,
-  (req: Request, res: Response) =>
-    productionOrderController.deliverProductionOrder(req, res),
-);
+// router.put(
+//   "/deliver/:production_order_id",
+//   getTokenMiddleware,
+//   (req: Request, res: Response) =>
+//     productionOrderController.deliverProductionOrder(req, res),
+// );
 router.patch(
   "/validate/:production_order_id",
   getTokenMiddleware,
