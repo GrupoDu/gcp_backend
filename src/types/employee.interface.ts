797824c1cd735bfa35b21bf94651322360ab4eb8
jukalbf <lgbr212@gmail.com>
2026-03-30 13:36:39 -0300

    feat(employee): Interface para teste
    Criando interface própria para testes

--- src/types/employee.interface.ts
+++ src/types/employee.interface.ts
@@ -14,3 +14,14 @@ export interface IEmployeeCreate extends Omit<IEmployee, "employee_id"> {}
 export interface IEmployeeUpdate extends Partial<
   Omit<IEmployee, "employee_id">
 > {}
+
+export interface IEmployeeTestType extends Omit<
+  IEmployee,
+  | "delivered_activities_quantity"
+  | "not_delivered_activities_quantity"
+  | "produced_quantity"
+> {
+  delivered_activities_quantity: number;
+  not_delivered_activities_quantity: number;
+  produced_quantity: number;
+}
