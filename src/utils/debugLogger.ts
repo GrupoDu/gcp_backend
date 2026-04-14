4c049541ee8484dbebb78cf91196dd23cd0d3ba2
jukalbf <lgbr212@gmail.com>
2026-04-14 17:05:22 -0300

    docs: add JSDoc comment to debugLogger function

--- src/utils/debugLogger.ts
+++ src/utils/debugLogger.ts
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+/**
+ * Logger para debug.
+ *
+ * Só é mostrando quando a variável de ambiente NODE_ENV for "development"
+ *
+ * @param {string[]} log - Array de strings para log
+ * @param {string} title - Título/contexto do log
+ */
 export default function debbugLogger(log: string[], title?: string) {
   const ENV = process.env.NODE_ENV;
   const isProd = ENV === "production";
